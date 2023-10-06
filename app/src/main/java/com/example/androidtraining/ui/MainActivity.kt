efbd6a5a182b7943bb18885b6869bf40dae9542f
Mohammed al sharif <ffmm972006@gmail.com>
2023-10-06 17:46:31 +0300

    new comit add

--- app/src/main/java/com/example/androidtraining/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidtraining/ui/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        println("Hello World What are you Ding")
         lifecycleScope.launch {
             collectLatestLifeCycle(viewModel.stateFlow) {
 
